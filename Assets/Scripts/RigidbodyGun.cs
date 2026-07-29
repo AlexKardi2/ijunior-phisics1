e66c105f3359cacf7869c896b571bc7dc0b20ed1
AlexKardi2 <alexkardi2@gmail.com>
2026-07-29 09:09:32 +0300

    input done

--- Assets/Scripts/RigidbodyGun.cs
+++ Assets/Scripts/RigidbodyGun.cs
@@ -6,32 +6,20 @@ public class RigidbodyGun : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbodyPrefab;
     [SerializeField] private Transform _startPosition;
     [SerializeField] private float _speed = 10f;
-    private float _timer = 1f;
-    private int _shoots = 15;
-    InputAction shootAction;
 
     private void Start()
     {
-        print("Shoot action have found is " + shootAction != null);
-        shootAction = InputSystem.actions.FindAction("Shoot");
-        print("Shoot action have found is " + shootAction != null);
-    }
-    private void Update()
-    {
-        if (shootAction.IsPressed())
-        {
-            Shoot();
-            _timer += 3f;
-            _shoots--;
-        }
-        else
-            _timer -= Time.deltaTime;
+
+
     }
 
-    private void Shoot()
+    public void Shoot(InputAction.CallbackContext context)
     {
+        if (context.performed == false)
+            return;
         Rigidbody projectile = Instantiate(_rigidbodyPrefab, _startPosition.position, _rigidbodyPrefab.rotation);
         Vector3 forward = _startPosition.forward;
         projectile.linearVelocity = forward * _speed;
     }
+
 }

473c32341dae5eb9464171e6a7d1179b555fc59a
AlexKardi2 <alexkardi2@gmail.com>
2026-08-10 08:59:13 +0300

    PlayerController

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-//TODO выделить управление игрока в отдельный слой
+//TODO выделить управление игрока в отдельный слой,  
 
 [RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 1;
     private Transform _transform;
     private CharacterController _characterController;
+    private InputAction _movementAction;
 
     private void Awake()
     {
         _transform = transform;
         _characterController = GetComponent<CharacterController>();
+        _movementAction = InputSystem.actions.FindAction("Movement");
     }
 
     private void Update()
     {
-        if (_characterController != null)
+        if (_characterController == null)
+            return;
+        Vector3 playerSpeed = _movementAction.ReadValue<Vector3>();
+        playerSpeed *= _speed * Time.deltaTime; 
+
+        if (_characterController.isGrounded)
         {
-            Vector3 playerInput = new Vector3();
+            _characterController.Move(playerSpeed + Vector3.down);
         }
+        else
+        {
+            _characterController.Move(_characterController.velocity + Physics.gravity * Time.deltaTime);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "Bath")
+            return;
+        hit.rigidbody.linearVelocity = Vector3.up * 100f;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        CharacterController character = GetComponent<CharacterController>();
+        Gizmos.DrawWireCube(transform.position, Vector3.right + Vector3.forward + Vector3.up * character.height);
     }
 }

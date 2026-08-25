1aee0724955d1e93d9c8b7f83ec3c35a5ba19773
AsusKardi <296899229+AsusTon@users.noreply.github.com>
2026-08-25 18:19:36 +0300

    Paint crosshair
    +optimized shotgun

--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class Shotgun : MonoBehaviour
 {
     [SerializeField] private float _damage = 10f;
+    [SerializeField] private float _maxDistance = 100f;
+    [SerializeField] LayerMask _layerMask;
     public void Shoot(Vector3 startPoint, Vector3 direction)
     {
         Debug.Log("Shooting from " + startPoint + " to " + direction);
+        if (Physics.Raycast(startPoint, direction, out RaycastHit hitInfo, _maxDistance, _layerMask, QueryTriggerInteraction.Ignore))
+        {
+
+        }
+
     }
 }

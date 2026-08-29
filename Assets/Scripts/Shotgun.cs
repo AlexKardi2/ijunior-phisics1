0247428cdb0bbefd1ff7ea3086fd78f978699c14
AlexKardi2 <alexkardi2@gmail.com>
2026-08-29 20:19:01 +0300

    Realize bullet decals
    - by imported png

--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -2,22 +2,39 @@ using UnityEngine;
 
 public class Shotgun : MonoBehaviour
 {
+    //R Создание префабов отделить в отдельный класс и вызывать его из оружия
+    //R Разделить скрипт на зоны ответственности. отдельно создание эффектов, отдельно нанесение урона, нанесение урона при помощи рейкастов - отдельным скриптом, т.е. оружие должно только вызывать в скрипте необходимость послать рейкаст
+    [SerializeField] private Transform _decalPrefab;
+    [SerializeField] private float _decalOffset=0.1f;
+    [SerializeField] private AudioSource _audioSource;
     [SerializeField] private float _damage = 10f;
     [SerializeField] private float _maxDistance = 100f;
     [SerializeField] private float _impactForce = 10f;
     [SerializeField] LayerMask _layerMask;
+
+    private void Start()
+    {
+        if (_decalPrefab == null)
+            throw new System.NullReferenceException("Decal prefab is not set");
+        if (_audioSource == null)
+            throw new System.NullReferenceException("Audio souis not set");
+    }
     public void Shoot(Ray shootRay)
     {
+        _audioSource.Play();
+        
         if (Physics.Raycast(shootRay, out RaycastHit hitInfo, _maxDistance, _layerMask, QueryTriggerInteraction.Ignore))
         {
+            Transform decal = Instantiate(_decalPrefab, hitInfo.transform);
+            decal.position = hitInfo.point + hitInfo.normal * _decalOffset;
+            decal.LookAt(hitInfo.point);
+            
             AbstractHealth health = hitInfo.collider.GetComponentInParent<AbstractHealth>();
             health?.TakeDamage(_damage);
 
             Rigidbody targetBody = hitInfo.rigidbody;
-            if (targetBody != null)
-            {
-                targetBody.AddForceAtPosition(shootRay.direction * _impactForce, hitInfo.point);
-            }
+            targetBody?.AddForceAtPosition(shootRay.direction * _impactForce, hitInfo.point);
+
         }
 
     }

b69c70f016109e8ee04b4f8453c4619551b28c7e
AlexKardi2 <alexkardi2@gmail.com>
2026-09-17 09:31:08 +0300

    Perlin texture 2

--- Assets/Scripts/Experimental/PerlinTexture.cs
+++ Assets/Scripts/Experimental/PerlinTexture.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class PerlinTexture : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //private const float OFFSET = 0.5f;
+    
+    [SerializeField] private Texture2D _texture;
+    [SerializeField] private MeshRenderer _renderer;
+    [SerializeField] [Range (0.0000001f, 3f)] private float _scale = 1f;
+    private float _oldScale = -1f;
+
+
+    private void Awake()
     {
-        
+        _texture = new Texture2D(100, 100);
+        _renderer.material.mainTexture = _texture;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _oldScale = _scale;
+
+        for (int x = 0; x < 100; x++)
+        {
+            for (int y = 0; y < 100; y++)
+            {
+                float grayLevel = Mathf.PerlinNoise(x*_scale+Time.time, y*_scale+Time.time);
+                _texture.SetPixel(x, y, new Color(grayLevel, grayLevel,grayLevel));
+            }
+        }
+
+
+        _texture.Apply();
     }
 }
